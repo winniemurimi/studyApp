6e35293986ab6ec88885f98e868d9202a57a3986
winnie <winniemurimi2@gmail.com>
2024-04-16 17:28:27 +0200

    nearlyDone

--- studentstudyportal/dashboard/forms.py
+++ studentstudyportal/dashboard/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . models import *
+from django.contrib.auth.forms import UserCreationForm
 
 class NotesForm(forms.ModelForm):
     class Meta:
@@ -15,3 +16,48 @@ class HomeworkForm(forms.ModelForm):
         model = Homework
         widgets = {'due':DateInput()}
         fields = ['subject', 'title', 'description', 'due', 'is_finished']
+
+class DashboardForm(forms.Form):
+    text = forms.CharField(max_length=100, label="Enter your search :")
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title', 'is_finished']
+
+class ConversionForm(forms.Form):
+    CHOICES = [('length','Length'),('mass','Mass')]
+    measurement = forms.ChoiceField(choices = CHOICES, widget=forms.RadioSelect)
+
+class ConversionLengthForm(forms.Form):
+    CHOICES = [('yard','Yard'),('foot', 'Foot')]
+    input = forms.CharField(required=False, label=False, widget=forms.TextInput(
+        attrs = {'type':'number','placeholder':'Enter the number'}
+    ))
+    measure1 = forms.CharField(
+        label ='',widget = forms.Select(choices = CHOICES)
+    )
+
+    measure2 = forms.CharField(
+        label ='',widget = forms.Select(choices = CHOICES)
+    )
+
+
+
+class ConversionMassForm(forms.Form):
+    CHOICES = [('pound','Pound'),('kilogram', 'Kilogram')]
+    input = forms.CharField(required=False, label=False, widget=forms.TextInput(
+        attrs = {'type':'number','placeholder':'Enter the number'}
+    ))
+    measure1 = forms.CharField(
+        label ='',widget = forms.Select(choices = CHOICES)
+    )
+
+    measure2 = forms.CharField(
+        label ='',widget = forms.Select(choices = CHOICES)
+    )
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','password1','password2']
--- studentstudyportal/studentstudyportal/settings.py
+++ studentstudyportal/studentstudyportal/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR/"static"]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
